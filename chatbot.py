e624992906afe45623bd9a641a85f9e0b966d2a6
Bhavesh-Sapar <bhaveshsapar07@gmail.com>
2026-01-07 13:34:19 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+# Loading Environment Variable
+load_dotenv()
+
+# Streamlit page setup
+st.set_page_config(
+    page_title = "Chatbot",
+    page_icon = "֎🇦🇮",
+    layout = "centered",
+)
+st.title("💬Generative AI Chatbot")
+
+# Initiate Chat History
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+# Show Chat History
+for message in st.session_state["chat_history"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Initiating LLM
+llm = ChatGroq(
+    model = "llama-3.1-8b-instant",
+    temperature = 0.0
+)
+
+user_prompt = st.chat_input("Ask Chatbot")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role" : "user", "content" : user_prompt})
+
+    response = llm.invoke(
+        input = [{"role" : "system", "content" : "You are a Helpful Assistant, be concise and accurate."}, *st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role" : "assistant", "content" : assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
